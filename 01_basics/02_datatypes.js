cfb11e313357a51f1166e915c148b01da24f8026
anussiddique508 <anussiddique508@gmail.com>
2026-08-21 13:44:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";  //use JS code as newer verion
+
+//alert(3+3) we are using nodejs , not browser
+
+let name = "anus"
+let age = 19 
+let isLooggedIn = true
+
+
+//numebr =>  2 to power 53
+//bigint =>  big integer
+//string =>  ""
+//boolean => true/false 
+//undefined => undefined 
+//null  => stand alone 
+//symbol => unique
+
+
+//object
+
+//typeof is used to identify data types.. 
+console.log(typeof undefined); //undefined is undefined
+console.log(typeof null); //null is object
