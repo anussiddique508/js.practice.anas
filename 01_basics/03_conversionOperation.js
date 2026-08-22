fdbfca1d6c779b7dbe89491c36514064536cea89
anussiddique508 <anussiddique508@gmail.com>
2026-08-22 11:04:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+//console.log(typeof score);
+//console.log (typeof(score));
+
+/*let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber)*/
+
+let valueInString= String (score)
+
+//console.log (typeof valueInString);
+//console.log (valueInString);
+
+//null type object
+//null print 0
+//undefined print NaN(not a number)
+//true print 1
+//false print 0
+//string print NaN
+
+
+let isloggedIn = "anas"
+let booleanisloggedIn = Boolean (isloggedIn);
+console.log(booleanisloggedIn);
+
+//conversion of boolean
+//1=> true ; 0=> false
+//""=> false 
+//"anas"=> true
+
