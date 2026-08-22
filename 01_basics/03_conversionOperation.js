d7286fbd83197c9e5c799324d1fc89d79c15aed7
anussiddique508 <anussiddique508@gmail.com>
2026-08-22 12:18:06 +0000

    conversion operation prefix/postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,10 +21,45 @@ let valueInString= String (score)
 
 let isloggedIn = "anas"
 let booleanisloggedIn = Boolean (isloggedIn);
-console.log(booleanisloggedIn);
+//console.log(booleanisloggedIn);
 
 //conversion of boolean
 //1=> true ; 0=> false
 //""=> false 
 //"anas"=> true
+
+/**************************************OPERATION**************************************/
+let value = 4
+let negvalue = -value
+//console.log (negvalue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3) used for power
+//console.log(2/2)
+//console.log(2%2)
+
+let str1 = "Anus"
+let str2 = " Siddique"
+//console.log(str1 + str2);
+
+// console.log("1" + 2);         //print 12
+// console.log(1 + "2");         //print 12
+// console.log("1" + 2 + 2);     //print 122
+// console.log(1 + 2 + "2");     //print 32
+
+//POSTFIX (first use then increase)
+let x = 3
+let y = x++
+console.log(x , y);
+
+//PREFIX (first increase then use)
+let a = 3
+let b = ++a
+console.log(a , b);
+
+
+
+
 
